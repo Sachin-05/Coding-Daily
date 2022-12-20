f64dd06e066f94ff3284cf5302825ce0320ec70e
Sachin-05 <51017352+Sachin-05@users.noreply.github.com>
2022-12-20 19:11:38 +0530

    Create Prime factorisation in O(n) and O(sqrt(n)).cpp

---
+++ Number Theory/Prime factorisation in O(n) and O(sqrt(n)).cpp
@@ -0,0 +1,49 @@
+// prime factorization
+// approach 1 using the o(n) complexity
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int n;
+   cin >> n;
+
+   for(int i=2; i<=n; ++i){
+    if(n%i==0){
+        int cnt=0;
+        while(n%i==0){
+            cnt++;
+            n = n/i;
+        }
+        cout << i << "^" << cnt << endl;
+    }
+   }
+}
+
+
+
+
+
+// approach 2 in time complexity of O(sqrt(n))
+
+#include <iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cin >> n;
+
+   for(int i=2; i*i<=n; ++i){
+    if(n%i==0){
+        int cnt=0;
+        while(n%i==0){
+            cnt++;
+            n = n/i;
+        }
+        cout << i << "^" << cnt << endl;
+    }
+   }
+   if(n>1){
+    cout << n << "^" << 1 << endl;
+   }
+}
